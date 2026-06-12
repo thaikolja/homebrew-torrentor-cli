4d73853c55b414f74622b6b1ab342656e9b45969
Kolja Nolte <kolja.nolte@gmail.com>
2026-06-12 08:30:10 +0700

    Fix: use pip install inside virtualenv for transitive deps

--- Formula/torrentor-cli.rb
+++ Formula/torrentor-cli.rb
@@ -10,23 +10,10 @@ class TorrentorCli < Formula
   depends_on "python@3.13"
   depends_on "transmission-cli"
 
-  resource "InquirerPy" do
-    url "https://files.pythonhosted.org/packages/source/I/InquirerPy/InquirerPy-0.3.4.tar.gz"
-    sha256 "89d2ada0111f337483cb41ae31073108b2ec1e618a49d7110b0d7ade89fc197e"
-  end
-
-  resource "typer" do
-    url "https://files.pythonhosted.org/packages/source/t/typer/typer-0.26.7.tar.gz"
-    sha256 "e314a34c617e419c091b2830dda3ea1f257134ff593061a8f5b9717ab8dddb3a"
-  end
-
-  resource "rich" do
-    url "https://files.pythonhosted.org/packages/source/r/rich/rich-15.0.0.tar.gz"
-    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
-  end
-
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec)
+    system venv.root/"bin/pip", "install", "torrentor-cli==#{version}"
+    bin.install_symlink venv.root/"bin/torrentor"
   end
 
   test do
