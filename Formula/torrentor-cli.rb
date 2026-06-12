ba98d530a31e3b0d3a9467e4fc52c29921480a82
Kolja Nolte <kolja.nolte@gmail.com>
2026-06-12 08:33:23 +0700

    Fix: add all transitive dependency resources

--- Formula/torrentor-cli.rb
+++ Formula/torrentor-cli.rb
@@ -10,10 +10,63 @@ class TorrentorCli < Formula
   depends_on "python@3.13"
   depends_on "transmission-cli"
 
+  resource "annotated-doc" do
+    url "https://files.pythonhosted.org/packages/source/a/annotated_doc/annotated_doc-0.0.4.tar.gz"
+    sha256 "fbcda96e87e9c92ad167c2e53839e57503ecfda18804ea28102353485033faa4"
+  end
+
+  resource "inquirerpy" do
+    url "https://files.pythonhosted.org/packages/source/I/InquirerPy/InquirerPy-0.3.4.tar.gz"
+    sha256 "89d2ada0111f337483cb41ae31073108b2ec1e618a49d7110b0d7ade89fc197e"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/source/m/markdown_it_py/markdown_it_py-4.2.0.tar.gz"
+    sha256 "04a21681d6fbb623de53f6f364d352309d4094dd4194040a10fd51833e418d49"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/source/m/mdurl/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "pfzy" do
+    url "https://files.pythonhosted.org/packages/source/p/pfzy/pfzy-0.3.4.tar.gz"
+    sha256 "717ea765dd10b63618e7298b2d98efd819e0b30cd5905c9707223dceeb94b3f1"
+  end
+
+  resource "prompt-toolkit" do
+    url "https://files.pythonhosted.org/packages/source/p/prompt_toolkit/prompt_toolkit-3.0.52.tar.gz"
+    sha256 "28cde192929c8e7321de85de1ddbe736f1375148b02f2e17edd840042b1be855"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/source/p/pygments/pygments-2.20.0.tar.gz"
+    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/source/r/rich/rich-15.0.0.tar.gz"
+    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
+  end
+
+  resource "shellingham" do
+    url "https://files.pythonhosted.org/packages/source/s/shellingham/shellingham-1.5.4.tar.gz"
+    sha256 "8dbca0739d487e5bd35ab3ca4b36e11c4078f3a234bfce294b0a0291363404de"
+  end
+
+  resource "typer" do
+    url "https://files.pythonhosted.org/packages/source/t/typer/typer-0.26.7.tar.gz"
+    sha256 "e314a34c617e419c091b2830dda3ea1f257134ff593061a8f5b9717ab8dddb3a"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/source/w/wcwidth/wcwidth-0.8.1.tar.gz"
+    sha256 "faf5b4a5366a72dc49cad48cdf21f52bdf63bdda995178e483ba247ff79089b9"
+  end
+
   def install
-    venv = virtualenv_create(libexec)
-    system venv.root/"bin/pip", "install", "torrentor-cli==#{version}"
-    bin.install_symlink venv.root/"bin/torrentor"
+    virtualenv_install_with_resources
   end
 
   test do
